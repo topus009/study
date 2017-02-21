ef07f42c8a1612f174f1743bf5b8373d79ba125f
Sergey Topolov <sj_89@mail.ru>
2017-02-21 14:51:29 +0300

    Mario

---
+++ 09/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+			case 37:
+				$('img').animate({left: "-=10px"}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 38:
+				$('img').animate({top: "-=10px"}, 'fast');
+				break;
+			// Right Arrow Pressed
+			case 39:
+				$('img').animate({left: "+=10px"}, 'fast');
+				break;
+			// Down Arrow Pressed
+			case 40:
+					$('img').animate({top: "+=10px"}, 'fast');
+				break;
+
+					case 65:
+				$('img').animate({left: "-=10px"}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 87:
+				$('img').animate({top: "-=10px"}, 'fast');
+				break;
+			// Right Arrow Pressed
+			case 68:
+				$('img').animate({left: "+=10px"}, 'fast');
+				break;
+			// Down Arrow Pressed
+			case 83:
+					$('img').animate({top: "+=10px"}, 'fast');
+				break;
+		}
+	});
+});
